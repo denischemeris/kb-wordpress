329ef3818d79b39f8b5bc4df56a86517c6cd9c08
denischemeris <denischemeris@gmail.com>
2026-03-18 23:39:32 +0300

    Add index.php, header.php, footer.php for theme support

---
+++ theme/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+    <div class="container">
+        <a class="navbar-brand" href="<?php echo home_url(); ?>">
+            <?php bloginfo( 'name' ); ?>
+        </a>
+        <div class="navbar-nav ms-auto">
+            <?php if ( is_user_logged_in() ) : ?>
+                <a class="nav-link" href="<?php echo home_url('/knowledge-base/'); ?>">База знаний</a>
+                <a class="nav-link" href="<?php echo wp_logout_url(home_url('/')); ?>">Выйти</a>
+            <?php else : ?>
+                <a class="nav-link" href="<?php echo home_url('/register/'); ?>">Регистрация</a>
+                <a class="nav-link" href="<?php echo home_url('/login/'); ?>">Вход</a>
+            <?php endif; ?>
+        </div>
+    </div>
+</nav>
---
+++ theme/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Main template file
+ *
+ * @package KB_Theme
+ */
+
+get_header();
+
+if ( have_posts() ) :
+    while ( have_posts() ) :
+        the_post();
+        the_content();
+    endwhile;
+endif;
+
+get_footer();
